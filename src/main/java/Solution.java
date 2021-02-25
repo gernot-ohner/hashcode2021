9ebd511dcb5e458122078995d76067e8043eee6c
Gernot Ohner <gernot.ohner@bearingpoint.com>
2021-02-25 19:58:14 +0100

    adds StreetCount

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,10 +1,28 @@
 import model.InputObject;
 import model.OutputObject;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class Solution {
     public OutputObject compute(InputObject inputObject) {
 
+        final var steetCounts = getSteetCounts(inputObject);
+
         final var outputObject = new OutputObject();
         return outputObject;
+    }
+
+    private TreeMap<Integer, String> getSteetCounts(InputObject inputObject) {
+        final var streetNames = inputObject.getCars().stream().flatMap(car -> car.getStreetsToTravel().stream()).collect(Collectors.toList());
+
+        final var steetCounts = new TreeMap<>(CollectionUtils.getCardinalityMap(streetNames));
+
+        var inverted = new TreeMap<Integer, String>();
+        steetCounts.descendingMap().forEach((s, integer) -> inverted.put(integer, s));
+
+        System.out.println(inverted.descendingMap());
+        return inverted;
     }
 }
